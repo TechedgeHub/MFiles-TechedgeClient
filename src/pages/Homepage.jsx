7b7a25e3ecd5e5f9278b07e4636b74db3a82e3be
Elai5 <yvettelaine007@gmail.com>
2025-07-16 01:52:45 +0300

    feat: add responsive steps card using react icons and lucide for select type, call,fill form and submit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,26 @@
 import React from "react";
 import Navbar from "../layout/Navbar";
 import { Button } from "@/components/ui/button";
-import { CircleCheck } from "lucide-react";
+import {
+  CircleCheck,
+  CircleChevronRight,
+  FileText,
+  Grid3X3Icon,
+  ListCheck,
+  SquareChartGantt,
+} from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import { FaLocationArrow } from "react-icons/fa6";
+import { RiFileList2Line } from "react-icons/ri";
+
+const StepsCard = ({ icon: Icon, label }) => (
+  <Card className="flex flex-col items-center justify-center p-4 gap-2 shadow-sm hover:shadow-md transition">
+    <CardContent className="flex flex-col items-center justify-center ">
+      <Icon className="w-8 h-8 text-primary" />
+      <p className="text-sm font-medium mt-2 text-center">{label}</p>
+    </CardContent>
+  </Card>
+);
 
 const Homepage = () => {
   return (
@@ -35,8 +54,20 @@ const Homepage = () => {
               </span>
               Fill dynamic metadata
             </li>
-            <li className="flex gap-2">submit & create</li>
+            <li className="flex gap-2">
+              <span>
+                {" "}
+                <CircleChevronRight />
+              </span>
+              submit & create
+            </li>
           </ul>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          <StepsCard icon={FileText} label="Select Type" />
+          <StepsCard icon={Grid3X3Icon} label="Choose Class" />
+          <StepsCard icon={RiFileList2Line} label="Fill Form" />
+          <StepsCard icon={FaLocationArrow} label="Submit" />
         </div>
       </div>
     </>
